b356a91c68353befe9cfb006e4baf6675b22b90b
Noelia <ncarus@vectoritcgroup.com>
2021-05-25 22:24:35 +0200

    commit

--- funciones.js
+++ funciones.js
@@ -1,24 +1,40 @@
 const PI = 3.14;
 
-function saludar () {
-console.log("Hola mundo");
+function saludar (mensaje="Hola mundo") {
+//console.log("Hola mundo");
+console.log(mensaje);
 }
 function saludarPorNombre(nombre, saludo = "Hola") {
-    console.log("Hola " + nombre) ;
+    //console.log("Hola " + nombre) ;
+    //console.log(`${saludo} ${nombre}`);
+    console.log(saludo , nombre); 
    }
+
+   
 saludar();
+saludar("Bienvenidos");
+let da = "Dani";
+saludarPorNombre(da);
 
-
- saludarPorNombre("Noelia");  
+saludarPorNombre("Noelia");  
+saludarPorNombre("Noelia", "Hello");
 
  let radio = 5;
+ // declaro una variable "circunferencia" para almacenar el valor devuelto por
+ // la función calcularCircunferencia pasando como argumento el valor de radio
  let circunferencia = calcularCircunferencia(radio);
-console.log(circunferencia);
+ //pinto el valor resultante
+console.log(" EL valor de la circunferencia es " + circunferencia);
 
  function calcularCircunferencia (r = 1){
+     /*
+     declaro una función para calcular la circunferencia con un argumento
+     r con valor por defecto =1.
+     y devuelvo un valor 
+     */
      const cir = 2 * PI * r;
      return cir;
 
  }
 
-
+ 
--- variables.js
+++ variables.js
@@ -4,7 +4,8 @@ const PI = 3.14;
 
 let rueda = 2 * PI * radio;
 
-// console.log( "rueda : + rueda );
+console.log( "La rueda mide : " + rueda );
+
 
 const bici = {
     marca: "BH",
@@ -13,5 +14,7 @@ const bici = {
     rueda: 2 * PI * radio,
 };
 
-console.log("Mi bici " + bici.marca + "tiene una rueda de" + bici.rueda + "cm.");
+console.log("Mi bici " + bici.marca + " tiene una rueda de " + bici.rueda + "cm.");
  
+bici.marca = "Orbea"; 
+console.log( " He comprado una bici nueva " + bici.marca + " porque la anterior estaba vieja"); 
