2bdcd338921c78b5d1a7fa215697b3c3a2134efd
Noelia <ncarus@vectoritcgroup.com>
2021-05-25 12:44:30 +0200

    commit

---
+++ iva.js
@@ -0,0 +1,6 @@
+let ivaNormal = 10;
+let precio = 50;
+
+let importe;
+importe = precio + (precio * ivaNormal) / 100;
+console.log ("el precio de  producto " + precio);
---
+++ variables.js
@@ -0,0 +1,17 @@
+let radio = 40;
+
+const PI = 3.14;
+
+let rueda = 2 * PI * radio;
+
+// console.log( "rueda : + rueda );
+
+const bici = {
+    marca: "BH",
+    precio: 89,
+    radio: radio,
+    rueda: 2 * PI * radio,
+};
+
+console.log("Mi bici " + bici.marca + "tiene una rueda de" + bici.rueda + "cm.");
+ 
